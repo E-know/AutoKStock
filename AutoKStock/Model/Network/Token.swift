711ad33cc95a2ec4ae8c9474d2a2f44a098901da
E-know <eknow25@kakao.com>
2024-02-19 18:00:22 +0900

    [수정] 코딩키 삭제

--- AutoKStock/Model/Network/Token.swift
+++ AutoKStock/Model/Network/Token.swift
@@ -7,31 +7,31 @@
 
 import Foundation
 
-struct IssueTokenInput: Encodable {
+struct IssueTokenBody: Encodable {
     let grant_type = "client_credentials"
     let appkey = Configuration.shared.AppKey
     let appsecret = Configuration.shared.AppSecret
 }
 
-struct IssueTokenOutput: Decodable {
+struct IssueTokenData: Decodable {
     let accessToken, accessTokenTokenExpired, tokenType: String
     let expiresIn: Int
 
-    enum CodingKeys: String, CodingKey {
-        case accessToken = "access_token"
-        case accessTokenTokenExpired = "access_token_token_expired"
-        case tokenType = "token_type"
-        case expiresIn = "expires_in"
-    }
+//    enum CodingKeys: String, CodingKey {
+//        case accessToken = "access_token"
+//        case accessTokenTokenExpired = "access_token_token_expired"
+//        case tokenType = "token_type"
+//        case expiresIn = "expires_in"
+//    }
 }
 
-struct RevokeTokenInput: Encodable {
+struct RevokeTokenBody: Encodable {
     let appkey = Configuration.shared.AppKey
     let appsecret = Configuration.shared.AppSecret
     let token: String
 }
 
-struct RevokeTokenOutput: Decodable {
+struct RevokeTokenData: Decodable {
     let code: Int
     let message: String
 }
--- AutoKStock/Model/Network/TradingVolumeRanking.swift
+++ AutoKStock/Model/Network/TradingVolumeRanking.swift
@@ -9,14 +9,7 @@ import Foundation
 
 struct TradingVolumeRankingData: Decodable {
     let output: [TradingVolumeRankingOutput]
-    let rtCD, msgCD, msg1: String
-
-    enum CodingKeys: String, CodingKey {
-        case output
-        case rtCD = "rt_cd"
-        case msgCD = "msg_cd"
-        case msg1
-    }
+    let rtCd, msgCd, msg1: String
 }
 
 struct TradingVolumeRankingOutput: Decodable {
@@ -25,26 +18,4 @@ struct TradingVolumeRankingOutput: Decodable {
     let prdyVol, lstnStcn, avrgVol, nBefrClprVrssPrprRate: String
     let volInrt, volTnrt, ndayVolTnrt, avrgTrPbmn: String
     let trPbmnTnrt, ndayTrPbmnTnrt, acmlTrPbmn: String
-
-    enum CodingKeys: String, CodingKey {
-        case htsKorIsnm = "hts_kor_isnm"
-        case mkscShrnIscd = "mksc_shrn_iscd"
-        case dataRank = "data_rank"
-        case stckPrpr = "stck_prpr"
-        case prdyVrssSign = "prdy_vrss_sign"
-        case prdyVrss = "prdy_vrss"
-        case prdyCtrt = "prdy_ctrt"
-        case acmlVol = "acml_vol"
-        case prdyVol = "prdy_vol"
-        case lstnStcn = "lstn_stcn"
-        case avrgVol = "avrg_vol"
-        case nBefrClprVrssPrprRate = "n_befr_clpr_vrss_prpr_rate"
-        case volInrt = "vol_inrt"
-        case volTnrt = "vol_tnrt"
-        case ndayVolTnrt = "nday_vol_tnrt"
-        case avrgTrPbmn = "avrg_tr_pbmn"
-        case trPbmnTnrt = "tr_pbmn_tnrt"
-        case ndayTrPbmnTnrt = "nday_tr_pbmn_tnrt"
-        case acmlTrPbmn = "acml_tr_pbmn"
-    }
 }
