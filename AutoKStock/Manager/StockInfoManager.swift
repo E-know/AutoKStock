3d1b4d57907753eb7708fc042978b27d8af83e8e
E-know <eknow25@kakao.com>
2024-02-20 00:12:24 +0900

    [추가] 주식 정보 파일 추가 및 StockInfoManager 추가

---
+++ AutoKStock/Manager/StockInfoManager.swift
@@ -0,0 +1,65 @@
+//
+//  StockInfoManager.swift
+//  AutoKStock
+//
+//  Created by Inho Choi on 2/19/24.
+//
+
+import Foundation
+
+class StockInfoManager {
+    private var numToName = [String: String]()
+    private var nameToNum = [String: String]()
+    private init() {
+        readCSV(name: "KOSPI")
+        readCSV(name: "KOSDAQ")
+    }
+    
+    static let shared = StockInfoManager()
+    
+    private func readCSV(name: String) {
+        if let url = Bundle.main.url(forResource: name, withExtension: "csv") {
+            do {
+                let csvString = try String(contentsOf: url)
+                let csvLines = csvString.components(separatedBy: .newlines)
+                for line in csvLines where line.count > 0 {
+                    let fields = line.components(separatedBy: ",")
+                    
+                    let name = fields[0]
+                    let key = String(format: "%06d", Int(fields[1])!)
+                    
+                    numToName[key] = name
+                    nameToNum[name] = key
+                }
+            } catch {
+                print("Failed to read \(name) file: \(error)")
+            }
+        } else {
+            print("\(name) File not found")
+        }
+    }
+    
+    func getStockFromName(productCode: String) throws -> StockInfo {
+        guard let num = Int(productCode) else { throw SearchError.codeIsNotNumber }
+        let productCode = String(format: "%06d", num)
+        guard let name = numToName[productCode] else { throw SearchError.notExist }
+        return StockInfo(productCode: productCode, name: name)
+    }
+    
+    func getStockFromCode(name: String) throws -> StockInfo {
+        guard let productCode = nameToNum[name] else { throw SearchError.notExist }
+        return StockInfo(productCode: productCode, name: name)
+    }
+}
+
+enum SearchError: LocalizedError {
+    case codeIsNotNumber
+    case notExist
+    
+    public var errorDescription: String? {
+        switch self {
+        case .codeIsNotNumber: "ProductCode가 숫자가 아닙니다."
+        case .notExist: "해당 정보가 존재하지 않습니다."
+        }
+    }
+}
---
+++ AutoKStock/Model/StockInfo.swift
@@ -0,0 +1,13 @@
+//
+//  StockInfo.swift
+//  AutoKStock
+//
+//  Created by Inho Choi on 2/19/24.
+//
+
+import Foundation
+
+struct StockInfo: Hashable {
+    let productCode: String
+    let name: String
+}
