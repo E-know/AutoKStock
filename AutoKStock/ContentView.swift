0eccbbff7d0a772b6c4b5172c54d0eea9960912f
E-know <eknow25@kakao.com>
2024-02-19 13:47:38 +0900

    [추가] SceneDelegate 추가

--- AutoKStock/ContentView.swift
+++ AutoKStock/ContentView.swift
@@ -5,15 +5,22 @@
 //  Created by Inho Choi on 2/16/24.
 //
 
+import AppKit
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.scenePhase) private var scenePhase
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            
+            Button("종료") {
+                NSApp.terminate(nil)
+            }
         }
         .padding()
         .task {
